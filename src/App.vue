aa57d5a70c02bbb35556edd34d1aabbbf600ce8c
chitochka <javascript@post.cz>
2021-05-13 14:07:29 +0200

    created template for : Main, Slovnik, Kurzy

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <template>
   <v-app app>
-    <!-- <MyMenu></MyMenu> -->
-    <!-- <v-app-bar app></v-app-bar> -->
 
     <v-main>
-      <v-container>
-        <!-- <SlovnikSeznam> </SlovnikSeznam> -->
-        <!-- Hello World <div> aa : {{danie}}</div> -->
+      <v-container >
 
-        <v-card>
+        <v-card >
           <v-tabs  v-model="tab"  background-color="deep-purple accent-4"  centered  dark  icons-and-text>
             <v-tabs-slider></v-tabs-slider>
-            <v-tab href="#tab-1">    Slovnik    <v-icon>mdi-flag-variant-outline</v-icon>  </v-tab>
-            <v-tab href="#tab-2">    KZT - CZK    <v-icon>mdi-currency-kzt</v-icon>  </v-tab>
-            <v-tab href="#tab-3">    Slider    <v-icon>mdi-arrow-collapse-up</v-icon>  </v-tab>
+            <v-tab href="#slovnik">    Slovnik    <v-icon>mdi-flag-variant-outline</v-icon>  </v-tab>
+            <v-tab href="#kurzy">    KZT - CZK    <v-icon>mdi-currency-kzt</v-icon>  </v-tab>
+            <v-tab href="#slider">    Slider    <v-icon>mdi-arrow-collapse-up</v-icon>  </v-tab>
           </v-tabs>
 
           <v-tabs-items v-model="tab">
-            <v-tab-item v-for="i in 3" :key="i" :value="'tab-' + i">
+            <!-- <v-tab-item v-for="i in 3" :key="i" :value="'tab-' + i">
               <v-card flat><v-card-text>{{ text }}</v-card-text></v-card>
+            </v-tab-item> -->
+            <v-tab-item :value="'slovnik' ">
+               <Slovnik></Slovnik>
             </v-tab-item>
+
+            <v-tab-item :value="'kurzy' ">
+               <Kurzy></Kurzy>
+            </v-tab-item>
+            <v-tab-item :value="'slider' ">
+               <Slider></Slider>
+            </v-tab-item>
+
           </v-tabs-items>
         </v-card>
       </v-container>
@@ -29,20 +36,21 @@
 
 
 <script>
-import SlovnikSeznam from "./components/SlovnikSeznam.vue";
 import MyMenu from "./components/Menu.vue";
+import Slovnik from "./components/Slovnik.vue";
+import Kurzy from "./components/Kurzy.vue";
+import Slider from "./components/Slider.vue"
 
 export default {
   name: "app",
   data() {
     return {
-      danie: "Danie",
       right: null,
       tab: null,
       text: 'lopem'
     };
   },
-  components: { SlovnikSeznam, MyMenu },
+  components: { Slovnik, MyMenu, Kurzy , Slider},
   created() {},
   mounted() {},
 };
---
+++ src/components/Slovnik.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-card flat>
+      <v-card-text>
+        <v-text-field v-model="text" label="slovo pro preklad" hide-details="auto" ></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+          <v-spacer></v-spacer>
+        <v-btn 
+            @click="translate" :disabled="text?false:true" 
+            :elevation="5" color="pink"> Přelož </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      text:""
+    }
+  },
+  // computed: {
+  //   disable:
+  // },
+  methods: {
+    translate() {
+      var url = "https://slovnik.seznam.cz/preklad/cesky_rusky/" + this.text;
+      window.open(url)
+    }
+  },
+};
+</script>
+
+
+<style scoped>
+
+</style>
